b004811e60f670067e21a9ac11c0bec23a744ade
gourav <gouravsharma20a@gmail.com>
2026-07-22 10:50:52 +0530

    feature: cahnged the nav searchbar placeholder name

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -85,7 +85,7 @@ export default function Navigation() {
             <input
               className="form-control me-2"
               type="search"
-              placeholder="Search by product"
+              placeholder="Search by category"
               aria-label="Search"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
